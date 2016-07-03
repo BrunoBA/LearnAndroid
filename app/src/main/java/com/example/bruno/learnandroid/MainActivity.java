d578139ed12798c0b860f654a65b513dfa89f300
brunoba <brunobarros2014@gmail.com>
2016-07-03 15:08:12 -0300

    Ajuste na chamada da segunda Activity

--- app/src/main/java/com/example/bruno/learnandroid/MainActivity.java
+++ app/src/main/java/com/example/bruno/learnandroid/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import javax.net.ssl.SSLSessionContext;
@@ -69,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
         t.show();
     }
 
-    public void callSecondAtctivity(){
+    public void callSecondActivity(View view){
 
         Intent i = new Intent( this, SecondActivity.class);
         startActivity(i);
